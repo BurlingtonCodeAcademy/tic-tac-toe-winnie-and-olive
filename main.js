e4770c5814d9e1e5c433d39a2af5ba047cdc900a
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-02-19 11:53:29 -0500

    winn

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ let playerXMove = document.getElementById("playerXMove");
 let compWins = 0;
 let playerWins = 0;
 
+let cell0 = document.getElementById("cell-0");
+let cell1 = document.getElementById("cell-1");
+let cell2 = document.getElementById("cell-2");
+let cell3 = document.getElementById("cell-3");
+let cell4 = document.getElementById("cell-4");
+let cell5 = document.getElementById("cell-5");
+let cell6 = document.getElementById("cell-6");
+let cell7 = document.getElementById("cell-7");
+let cell8 = document.getElementById("cell-8");
+
 // changing button color on click (graying it out)
 //BUTTON DISAPPEARS NEED TO FIX !!!
 // let gameStart = start.addEventListener('click', (evt) => {
@@ -34,6 +44,8 @@ let status = document.getElementById("status");
 let gameActive = true; //whether game is restarted
 let currentPlayer = "X"; //this will be changed, but starts with player/user X
 let playedCells = ["", "", "", "", "", "", "", "", ""];
+let lowerString1 = "X";
+let lowerString2 = "O";
 
 gameStart();
 
@@ -67,8 +79,8 @@ function newPlayerTurn(evt) {
     status.textContent = `It's ${lowerString2}'s turn!`;
     console.log(lowerString2);
   }
+  winCheck();
 }
-///NOT WORKING HEHEHE LOWER STRINGS NOT DEFINEDDDD HOW R WE FOOKIN UP
 
 // name grabber
 let title = document.getElementById("title");
@@ -79,8 +91,8 @@ inputForm.addEventListener("submit", (evt) => {
   let userInput1 = document.getElementById("name1").value;
   let userInput2 = document.getElementById("name2").value;
   //name 1 & 2 input --- deleted .trim() after lowercase
-  let lowerString1 = userInput1.toString().toLowerCase();
-  let lowerString2 = userInput2.toString().toLowerCase();
+  lowerString1 = userInput1.toString().toLowerCase();
+  lowerString2 = userInput2.toString().toLowerCase();
   title.textContent = `Hello, ${lowerString1} and ${lowerString2}!`;
 });
 
@@ -99,17 +111,35 @@ function selectedCell(clickedCell, cellIndex) {
 }
 // if selectedCell != '0', then alert(please select an empty cell) & returns to firstTurn (aka their turn doesn't end)
 
-// const winningConditions = [
-//   [0, 1, 2],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [2, 4, 6],
-//   [6, 7, 8],
-//   [3, 4, 5],
-//   [0, 4, 8],
-// ];
+///WINNING CONDITIONS COMMENTED OUT FOR NOWWW
 
+const winningConditions = [
+  [cell0, cell1, cell2],
+  [cell0, cell3, cell6],
+  [cell1, cell4, cell7],
+  [cell2, cell5, cell8],
+  [cell2, cell4, cell6],
+  [cell6, cell7, cell8],
+  [cell3, cell4, cell5],
+  [cell0, cell4, cell8],
+];
+
+function winCheck(){
+  console.log("winnningcheck")
+for (let win of winningConditions) {
+  if (
+    win[0].textContent !== "" &&
+    win[1].textContent !== "" &&
+    win[2].textContent !== "" &&
+  win[0].textContent === win[1].textContent && win[0].textContent === win[2].textContent
+   ) {
+    window.alert("you win!");
+  }
+  
+}}
+
+
+// if (playedCells[selectedCell] !== '' )
 // let win1 = ['cell-0', 'cell-1', 'cell-2']
 // let cellArray = ["X"]
 
@@ -117,36 +147,33 @@ function selectedCell(clickedCell, cellIndex) {
 // cellArray.push("X")
 // }
 
-function winningMessage () => {
-if(cell1.textContent === cell2.textContent && cell1.textContent === cell0.textContent) {
-  window.alert ("you win!")
-} 
-if (cell0.textContent === cell3.textContent && cell0.textContent === cell6.textContent) {
-  window.alert ("you win!")
-}
-if (cell1.textContent === cell4.textContent && cell1.textContent === cell7.textContent) {
-  window.alert ("you win!")
-}
-if (cell2.textContent === cell5.textContent && cell2.textContent === cell8.textContent) {
-  window.alert ("you win!")
-}
-if (cell2.textContent === cell4.textContent && cell2.textContent === cell6.textContent) {
-  window.alert ("you win!")
-}
-if (cell6.textContent === cell7.textContent && cell6.textContent === cell8.textContent) {
-  window.alert ("you win!")
-}
-if (cell3.textContent === cell4.textContent && cell3.textContent === cell5.textContent) {
-  window.alert ("you win!")
-}
-if (cell0.textContent === cell4.textContent && cell0.textContent === cell8.textContent) {
-  window.alert("you win!")
-} else if {
-  window.alert ("you lose!")
-}
-
-
-
+//  winningMessage () => {
+// if(cell1.textContent === cell2.textContent && cell1.textContent === cell0.textContent) {
+//   window.alert ("you win!")
+// }
+// if (cell0.textContent === cell3.textContent && cell0.textContent === cell6.textContent) {
+//   window.alert ("you win!")
+// }
+// if (cell1.textContent === cell4.textContent && cell1.textContent === cell7.textContent) {
+//   window.alert ("you win!")
+// }
+// if (cell2.textContent === cell5.textContent && cell2.textContent === cell8.textContent) {
+//   window.alert ("you win!")
+// }
+// if (cell2.textContent === cell4.textContent && cell2.textContent === cell6.textContent) {
+//   window.alert ("you win!")
+// }
+// if (cell6.textContent === cell7.textContent && cell6.textContent === cell8.textContent) {
+//   window.alert ("you win!")
+// }
+// if (cell3.textContent === cell4.textContent && cell3.textContent === cell5.textContent) {
+//   window.alert ("you win!")
+// }
+// if (cell0.textContent === cell4.textContent && cell0.textContent === cell8.textContent) {
+//   window.alert("you win!")
+// } else if {
+//   window.alert ("you lose!")
+// }
 
 //else lose message
 //}
@@ -155,40 +182,40 @@ if (cell0.textContent === cell4.textContent && cell0.textContent === cell8.textC
 //and then disable the buttons once they are entered...
 
 // when user clicks a box, an X appears
-let cell0 = document.getElementById("cell-0");
+// let cell0 = document.getElementById("cell-0");
 cell0.addEventListener("click", (evt) => {
   selectedCell(cell0, 0);
   //playerTurn = "O";
 });
-let cell1 = document.getElementById("cell-1");
+// let cell1 = document.getElementById("cell-1");
 cell1.addEventListener("click", (evt) => {
   selectedCell(cell1, 1);
 });
-let cell2 = document.getElementById("cell-2");
+// let cell2 = document.getElementById("cell-2");
 cell2.addEventListener("click", (evt) => {
   selectedCell(cell2, 2);
 });
-let cell3 = document.getElementById("cell-3");
+// let cell3 = document.getElementById("cell-3");
 cell3.addEventListener("click", (evt) => {
   selectedCell(cell3, 3);
 });
-let cell4 = document.getElementById("cell-4");
+// let cell4 = document.getElementById("cell-4");
 cell4.addEventListener("click", (evt) => {
   selectedCell(cell4, 4);
 });
-let cell5 = document.getElementById("cell-5");
+// let cell5 = document.getElementById("cell-5");
 cell5.addEventListener("click", (evt) => {
   selectedCell(cell5, 5);
 });
-let cell6 = document.getElementById("cell-6");
+// let cell6 = document.getElementById("cell-6");
 cell6.addEventListener("click", (evt) => {
   selectedCell(cell6, 6);
 });
-let cell7 = document.getElementById("cell-7");
+// let cell7 = document.getElementById("cell-7");
 cell7.addEventListener("click", (evt) => {
   selectedCell(cell7, 7);
 });
-let cell8 = document.getElementById("cell-8");
+// let cell8 = document.getElementById("cell-8");
 cell8.addEventListener("click", (evt) => {
   selectedCell(cell8, 8);
 });
